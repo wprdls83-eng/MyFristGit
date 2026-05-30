685fb1b92f4a862d09c08ee3dcc02ff3ee676eb7
Yesman <79469058+Yesssssman@users.noreply.github.com>
2026-05-30 14:27:14 +0900

    A커밋
    A커밋

--- Assets/One.cs
+++ Assets/One.cs
@@ -1,10 +1,12 @@
-﻿using UnityEngine;
+using UnityEngine;
 
 public class One : MonoBehaviour
 {
     void Start()
     {
-        // 안녕
+        // 안녕123123
+        int damage = 10;
+        Debug.Log("데미지 : " + damage);
     }
 
     void Update()
